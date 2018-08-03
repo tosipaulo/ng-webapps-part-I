cb51bade228647c34d385b2051bc5807200c6c90
Paulo Tosi <tosi.paulo@gmail.com>
2018-08-02 23:46:59 -0300

    Criando onInit e post http

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { FotoComponent } from './../foto/foto.component';
-
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -11,8 +11,10 @@ import { FotoComponent } from './../foto/foto.component';
 export class CadastroComponent {
     
     foto: FotoComponent = new FotoComponent();
+    http: Http;
 
-    constructor() {
+    constructor(http: Http) {
+        this.http = http;
         this.foto.titulo = 'A';
         this.foto.url = 'B';
         this.foto.descricao = 'C';
@@ -20,7 +22,17 @@ export class CadastroComponent {
 
     cadastrar(event) {
         event.preventDefault();
-        console.log(this.foto)
+        console.log(this.foto);
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+
+        this.http.post('v1/fotos', JSON.stringify(this.foto), { headers: headers })
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log('Foto salva com sucesso')
+            }, err => console.log(err))
     }
 
 }
